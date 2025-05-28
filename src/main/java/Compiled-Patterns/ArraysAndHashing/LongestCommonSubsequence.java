e7645573baf1730d0a0217f5d0bbd4cbfe598964
Souvik943 <souvikkarmakar2k19@gmail.com>
2025-05-28 20:38:08 +0530

    File re-structure

---
+++ src/main/java/Compiled-Patterns/ArraysAndHashing/LongestCommonSubsequence.java
@@ -0,0 +1,41 @@
+package Neetcode.Patterns.ArraysAndHashing;
+
+import java.util.HashSet;
+import java.util.Set;
+//LC : 128
+public class LongestCommonSubsequence {
+
+//    TC - O(n)
+//    SC - O(n)
+//1. Maintain a maxLength variable
+//2. Store all elements to set
+//3. Iterate through set
+//4. Check :
+//    - For a single element in the set , we need to :
+//    - If its prev number is not in the set , then
+//    - we need to check further that if its next number is present in the set , If yes , then we count the length .
+//    - We maintain a while loop , till the set contains the next elemet for current element and increase the count accordingly .
+    public static int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        int maxLength = 0;
+        for(int i : nums) {
+            set.add(i);
+        }
+        for(int i : set) {
+            if(!set.contains(i-1)) {
+                int currentValue = i;
+                int currentLength = 1;
+                while(set.contains(currentValue + 1)) {
+                    currentValue++;
+                    currentLength++;
+                }
+                maxLength = Math.max(currentLength, maxLength);
+            }
+        }
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestConsecutive(new int[]{100,4,200,1,3,2}));
+    }
+}
---
+++ src/main/java/Compiled-Patterns/ArraysAndHashing/ValidSudoku.java
@@ -0,0 +1,31 @@
+package Neetcode.Patterns.ArraysAndHashing;
+
+import java.util.HashSet;
+//LC : 36
+public class ValidSudoku {
+//1. We need to initialise a hashset of String
+//2. We go though the char array by using 2 loops
+//3. If the char is '.' , we continue
+//4. We need to understand that hashSet.add(currentChar + "at row" + i) returns a true , if its not present
+//5. Now we defined a hashset of string type , because we will be adding the string like :
+//            seenAt.add(currentChar + "at row" + i)
+//            seenAt.add(currentChar + "at column" + j)
+//            seenAt.add(currentChar + "at subbox" + i/3 + "-" + j/3)
+//            It will always give unique values and will not throw an error .
+
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> seenAt = new HashSet<>();
+        for(int i=0; i<9; i++) {
+            for(int j=0; j<9; j++) {
+                char currentChar = board[i][j];
+                if(currentChar == '.') continue;
+                if(!seenAt.add(currentChar + "at row" + i) ||
+                        !seenAt.add(currentChar + "at column" + j) ||
+                        !seenAt.add(currentChar + "at subbox" + i/3 + "-" + j/3)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
