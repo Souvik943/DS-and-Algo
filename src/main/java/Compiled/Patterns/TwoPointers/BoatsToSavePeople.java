b4a4d7dabdc4ab4ed96fdcc23cb02e55d742fe16
Souvik943 <souvikkarmakar2k19@gmail.com>
2025-05-29 21:36:38 +0530

    LeetCode Question : Boats to Save people

--- src/main/java/Compiled/Patterns/TwoPointers/BoatsToSavePeople.java
+++ src/main/java/Compiled/Patterns/TwoPointers/BoatsToSavePeople.java
@@ -18,7 +18,7 @@ public class BoatsToSavePeople {
         int totalBoatCount = 0;
         int left = 0;
         int right = people.length-1;
-        while(left < right) {
+        while(left <= right) {
             if(people[left] + people[right] <= limit) left++;
             right--;
             totalBoatCount++;
